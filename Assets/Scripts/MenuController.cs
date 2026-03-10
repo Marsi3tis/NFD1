a55296a56e9ff04ab13a0205fa551ea15727b0bb
LudwigTheStick <matazer963@gmail.com>
2026-03-10 13:30:29 +0200

    padarytas ui
    galima ieiti ir iseiti is zaidimo, paziureti credits, settings (tuscia), iseiti is zaidimo

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,51 +4,104 @@ public class MenuController : MonoBehaviour
 {
     [Header("UI Settings")]
     [SerializeField] private GameObject menuPanel;
-
+    bool isActive = false;
+   bool Pausedgame = true;
     void Start()
     {
+
+        if (Pausedgame == true)
+        {
+            Time.timeScale = 0f;
+            Debug.Log("Game is paused");
+            
+            
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Debug.Log("Game is not paused");
+            
+            
+        }
+       
+        /*if (menuPanel != null)
+        {
+            menuPanel.SetActive(false);
+        }*/
+       
+       /* if (Time.timeScale == 0f)
+        {
+            ToggleMenu();
+            return; // Exit Start early
+        }*/
+
         Debug.Log("TimeScale: " + Time.timeScale);
+
         // Always start the game running
-        Time.timeScale = 1f;
+        /*if (isActive)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }*/
 
         // Make sure pause menu is hidden
-        if (menuPanel != null)
-            menuPanel.SetActive(false);
-        Debug.Log("TimeScale: " + Time.timeScale);
+        /*if (menuPanel != null)
+            menuPanel.SetActive(false);*/
+
+        Debug.Log("TimeScale1: " + Time.timeScale);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ToggleMenu();
-            Debug.Log("TimeScale: " + Time.timeScale);
+            
+            PauseGame();
+            Debug.Log("TimeScale2: " + Time.timeScale);
         }
     }
 
     void ToggleMenu()
     {
+        
         if (menuPanel == null) return;
-
-        bool isActive = menuPanel.activeSelf;
+        
+        
+        isActive = menuPanel.activeSelf;
 
         if (isActive)
             ResumeGame();
         else
             PauseGame();
-        Debug.Log("TimeScale: " + Time.timeScale);
+
+        Debug.Log("TimeScale3: " + Time.timeScale);
     }
 
     void PauseGame()
     {
+        
         Time.timeScale = 0f;
         menuPanel.SetActive(true);
-
     }
 
     void ResumeGame()
     {
+        
         Time.timeScale = 1f;
         menuPanel.SetActive(false);
+    }
+    public void OnResumeButtonPressed()
+    {
+        ResumeGame();
+        Debug.Log("Resumed Game");
+        
+    }
+    public void OnPlayButtonPressed()
+    {
+        ResumeGame();
+        Debug.Log("Playing game");
     }
 }
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
